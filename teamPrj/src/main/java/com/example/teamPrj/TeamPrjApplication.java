0319e32367b2bbd05d5e2900411c4a40392a91bc
Moon SongI <msongi061@gmail.com>
2024-05-02 11:05:07 +0900

    파일 초기 설정

---
+++ teamPrj/src/main/java/com/example/teamPrj/TeamPrjApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamPrj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamPrjApplication.class, args);
+	}
+
+}
---
+++ teamPrj/src/main/java/com/example/teamPrj/controller/MyController.java
@@ -0,0 +1,14 @@
+package com.example.teamPrj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+	
+	@RequestMapping("/")
+	public @ResponseBody String root() {
+		return "hello";
+	}
+}
